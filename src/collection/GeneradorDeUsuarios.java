c5684684fde893b9323ac11e87b10804a0bae405
T-107 <T-107@PC264>
2015-07-13 19:43:39 -0500

    collection13-07

--- src/collection/GeneradorDeUsuarios.java
+++ src/collection/GeneradorDeUsuarios.java
@@ -18,8 +18,13 @@ public class GeneradorDeUsuarios {
         Usuario u1=new Usuario("Cesar", 23, "ck@stark.com");
         Usuario u2=new Usuario("Pau", 22, "pau@gmail.com");
         Usuario u3=new Usuario("Raul", 24, "raul@gmail.com");
+        Usuario u4=new Usuario("Pamcha", 60, "pancha@gmail.com");
         
         usuarios=new ArrayList<Usuario>(); 
+        usuarios.add(u1);
+        usuarios.add(u2);
+        usuarios.add(u3);
+        usuarios.add(u4);
     }
 
     public List<Usuario> getUsuarios() {
@@ -30,6 +35,10 @@ public class GeneradorDeUsuarios {
         this.usuarios = usuarios;
     }
     
-
+    public String agregarUsuario(String nombre, int edad, String email){
+        Usuario u=new Usuario(nombre, edad, email);
+        usuarios.add(u);
+        return "Usuario Agregado con Exito";
+    }
     
 }
