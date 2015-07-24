41d66864f7c2e484e29f057845738256bc3032d0
T-107 <T-107@PC264>
2015-07-23 19:47:31 -0500

    23/07 Revisar !!

--- src/serializacion/ModeloPersistenciaAnimal.java
+++ src/serializacion/ModeloPersistenciaAnimal.java
@@ -9,15 +9,29 @@ import java.io.*;
 
 public class ModeloPersistenciaAnimal {
     
-    
+    String ruta="D:\\animalitos.xxx";
     
     public void guardar(Animal animal) throws Exception{
         //paso 1 para crear el archivo donde se guarda el animal
-        File file=new File("D:\\animalitos.xxx");
+        File file=new File(ruta);
+        if(file.exists()){
+            
+        }
        FileOutputStream fos=new FileOutputStream(file);
        ObjectOutputStream oss=new ObjectOutputStream(fos);
        oss.writeObject(animal);
         System.out.println("Animalito comprimido con exito");
+        
+    }
+    
+    public Animal buscarTodos() throws Exception{
+        Animal a=new Animal();
+        File f=new File(ruta);
+        FileInputStream ois= new FileInputStream (f);
+        ObjectInputStream ois=new ObjectInputStream(fos);
+            a=(Animal) ois.readObject();
+            return a;
+        
         
     }
     
---
+++ src/serializacion/Zoologico.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializacion;
+
+/**
+ *
+ * @author T-107
+ */
+public class Zoologico {
+    public static void main(String[] args) throws Exception{
+        ModeloPersistenciaAnimal m=new ModeloPersistenciaAnimal();
+       Animal a= m.buscarTodos();
+        System.out.println(a.getNombre());
+    }
+    
+}
